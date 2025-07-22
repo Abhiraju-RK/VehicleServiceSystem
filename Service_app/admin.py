8186830a1538d5733ab75420c5cbf90bf40ef7fa
Abhiraju R.K <abhiraju5984@gmail.com>
2025-07-22 12:24:57 +0530

    Update admin.py

--- Service_app/admin.py
+++ Service_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . models import *
 # Register your models here.
+
+admin.site.register(Vehicle)
+admin.site.register(Booking)
+admin.site.register(GarageProfile)
+admin.site.register(Service)
+admin.site.register(Complaint)
